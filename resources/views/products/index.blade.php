1ddcedc6f56309d37b6204b5e9a15dcad281d91e
fluoh <work@fluoh.fr>
2022-02-23 10:10:17 +0100

    premier commit

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,54 @@
+@extends('products.layout')
+ 
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h3>Laravel 8 CRUD Application Tutorial</h3>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-success" href="{{ route('products.create') }}"> Add New Product</a>
+            </div>
+        </div>
+    </div>
+   
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+   
+    <table class="table table-bordered" style="margin-top:20px;">
+        <tr>
+            <th>No</th>
+            <th>Name</th>
+            <th>Amount</th>
+            <th>Description</th>
+            <th width="280px">Action</th>
+        </tr>
+        @foreach ($products as $product)
+        <tr>
+            <td>{{ ++$i }}</td>
+            <td>{{ $product->name }}</td>
+            <td>{{ $product->amount }}</td>
+            <td>{{ $product->description }}</td>
+            <td>
+                <form action="{{ route('products.destroy',$product->id) }}" method="POST">
+   
+                    <a class="btn btn-info" href="{{ route('products.show',$product->id) }}">Show</a>
+    
+                    <a class="btn btn-primary" href="{{ route('products.edit',$product->id) }}">Edit</a>
+   
+                    @csrf
+                    @method('DELETE')
+      
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </table>
+  
+    {!! $products->links() !!}
+      
+@endsection
